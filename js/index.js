1f24831d08a553ba063d64f9f0fe4caa60409be9
Pamplemousse <xavier.maso@net-c.com>
2016-02-09 20:10:12 +1300

    Move the js into it's own file and...
      * reorder some declarations to make the logic more explict
      * add comment to 'explain' the wiring between leapmotion api and Rx
      * correct some JS styles according to 'standard'

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var frames = []
+
+var frameChanges$ = Rx.Observable.ofArrayChanges(frames)
+
+// Let the leap api's infinite loop update the frames array
+Leap.loop({ enableGestures: true }, function (frame) {
+  frames.push(frame)
+})
+
+var lastFrames$ = frameChanges$
+  .map(function (frameChange) {
+    return _.last(frameChange.object)
+  })
+
+var hands$ = lastFrames$
+  .filter(function (frame) {
+    return !_.isEmpty(frame.hands)
+  })
+  .map(function (frame) {
+    return frame.hands[0]
+  })
+
+hands$.subscribe(
+   function (hand) {
+     console.log(hand)
+   }
+)
