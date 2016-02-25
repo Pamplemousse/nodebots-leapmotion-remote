bb97ef92853bcb7cfe20de77c2f256c5846524da
Pamplemousse <xavier.maso@net-c.com>
2016-02-25 21:58:57 +1300

    Add visual feedback for all directions
    This front end management is pretty ugly, but did not want to dive
    into something overly designed (hahaha, already 2 modules) for just
    some visual feedback.

--- app/main.js
+++ app/main.js
@@ -10,5 +10,78 @@ hands.leftHand$.subscribe(
 hands.noHands$.subscribe(
   function () {
     tiles.toggle('off')
+    tiles.changeBackground('onOff', 'red')
+    tiles.changeBackground('up', 'lightblue')
+    tiles.changeBackground('right', 'lightblue')
+    tiles.changeBackground('left', 'lightblue')
+  }
+)
+
+var forwardDirection$ = hands.leftHand$
+  .filter(function (hand) {
+    return hand.direction[1] < -0.20
+  })
+
+// Ugly, here only for display purposes
+var notForwardDirection$ = hands.leftHand$
+  .filter(function (hand) {
+    return hand.direction[1] > -0.20
+  })
+
+var rightDirection$ = hands.leftHand$
+  .filter(function (hand) {
+    return hand.direction[0] > 0.20
+  })
+
+// Ugly, here only for display purposes
+var notRightDirection$ = hands.leftHand$
+  .filter(function (hand) {
+    return hand.direction[0] < 0.20
+  })
+
+var leftDirection$ = hands.leftHand$
+  .filter(function (hand) {
+    return hand.direction[0] < -0.20
+  })
+
+// Ugly, here only for display purposes
+var notLeftDirection$ = hands.leftHand$
+  .filter(function (hand) {
+    return hand.direction[0] > -0.20
+  })
+
+forwardDirection$.subscribe(
+  function () {
+    tiles.changeBackground('up', 'blue')
+  }
+)
+
+notForwardDirection$.subscribe(
+  function () {
+    tiles.changeBackground('up', 'lightblue')
+  }
+)
+
+rightDirection$.subscribe(
+  function () {
+    tiles.changeBackground('right', 'blue')
+  }
+)
+
+notRightDirection$.subscribe(
+  function () {
+    tiles.changeBackground('right', 'lightblue')
+  }
+)
+
+leftDirection$.subscribe(
+  function () {
+    tiles.changeBackground('left', 'blue')
+  }
+)
+
+notLeftDirection$.subscribe(
+  function () {
+    tiles.changeBackground('left', 'lightblue')
   }
 )
